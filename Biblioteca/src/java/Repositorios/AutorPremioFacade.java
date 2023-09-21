0cb4b40b387617eb49f88d2f3f59a32b35238b00
Alberto Saz <albertosaz@gmail.com>
2023-09-21 10:46:50 +0200

    persistencia vista-controlador-repositorio

---
+++ Biblioteca/src/java/Repositorios/AutorPremioFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repositorios;
+
+import Modelos.AutorPremio;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author alberto
+ */
+@Stateless
+public class AutorPremioFacade extends AbstractFacade<AutorPremio> {
+
+    @PersistenceContext(unitName = "BibliotecaPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AutorPremioFacade() {
+        super(AutorPremio.class);
+    }
+    
+}
