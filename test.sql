a1ec38e8bef2afa8ee86a3e9787e8e31caa9ff13
Alberto Saz <albertosaz@gmail.com>
2023-11-27 13:00:04 +0100

    proyecto examen bien

---
+++ test.sql
@@ -0,0 +1,15 @@
+SELECT p.codigo,
+       e.codigo
+FROM Proyectos as p
+cross join Entidades as e
+    ON p.entidad = e.codigo
+where p.entidad not in
+(select e1.codigo
+from Entidades e1
+);
+
+select p.entidad
+from Proyectos p;
+
+select e.codigo
+from Entidades e;
