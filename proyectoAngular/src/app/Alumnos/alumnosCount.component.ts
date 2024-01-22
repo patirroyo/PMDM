72c8ba67a19623144d308b0e47b22f612fa17641
Alberto Saz <albertosaz@gmail.com>
2024-01-22 13:19:55 +0100

    formsmodule

--- proyectoAngular/src/app/Alumnos/alumnosCount.component.ts
+++ proyectoAngular/src/app/Alumnos/alumnosCount.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
     selector: 'cuantos-alumnos',
     standalone: true,
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, FormsModule],
     templateUrl: './alumnosCount.component.html',
     styleUrl: './alumnosCount.component.css'
 })
@@ -20,4 +21,11 @@ export class AlumnosCountComponent {
 
     @Input()
     mujeres: number = 0;
+
+    //Esto sirve para que se pueda pasar el valor de la variable desde el componente hijo al padre y se carguen los atributos; se pone el @Output() delante de la variable para que se pueda pasar el valor
+    
+    opcionElegida : string = "Todos";
+    
+    @Output()
+    globalElegido : EventEmitter<string> = new EventEmitter<string>();
 }
