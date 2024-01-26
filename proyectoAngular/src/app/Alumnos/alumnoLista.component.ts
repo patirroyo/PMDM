c3c89a390fb27fb25483cee77ecff382e612abdb
Alberto Saz <albertosaz@gmail.com>
2024-01-26 13:04:48 +0100

    AmbarExport

--- proyectoAngular/src/app/Alumnos/alumnoLista.component.ts
+++ proyectoAngular/src/app/Alumnos/alumnoLista.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AlumnoTituloPipe } from './alumnoTitulo.pipe';
 import { AlumnosCountComponent } from './alumnosCount.component';
+import { IAlumno, Alumno } from './alumno';
 
 
 @Component({
     selector: 'lista-alumnos',
     standalone: true,
     imports: [CommonModule, RouterOutlet, AlumnoTituloPipe, AlumnosCountComponent],
-    templateUrl: './alumnoLista.component.html', 
+    templateUrl: './alumnoLista.component.html',
     styleUrl: './alumno.component.css'
 })
 
@@ -18,13 +19,16 @@ export class AlumnoListaComponent {
     //recibe el valor del hijo
 
     seleccion: string = 'Todos';//Esto es para que por defecto se muestren todos los alumnos, pero se puede cambiar a Hombres o Mujeres por medio del select que envia un evento al padre(donde estamos) y se cambia el valor de la variable seleccion
-
-    alumnos: any[] = [
-        {nombre: 'Perico', apellidos: 'Delgado', direccion: 'Segovia', fnac: '2/28/1958', sexo: 'Hombre'},
-        {nombre: 'Miguel', apellidos: 'Indurain', direccion: 'Pamplona', fnac: '10/2/1968', sexo: 'Hombre'},
-        {nombre: 'Alberto', apellidos: 'Contador', direccion: 'Madrid', fnac: '05/04/1980', sexo: 'Hombre'},
-        {nombre: 'María José', apellidos: 'Jiménez', direccion: 'Cuenca', fnac: '12/30/1981', sexo: 'Mujer'},
-    ];
+    //alumnos: IAlumno[];//Esto es un array de objetos de tipo IAlumno, que es una interfaz que hemos creado en alumno.ts y tengo que importarla arriba
+    alumnos: Alumno[];
+    constructor() {
+        this.alumnos = [
+            { nombre: 'Perico', apellidos: 'Delgado', direccion: 'Segovia', fnac: '2/28/1958', sexo: 'Hombre', nom_padre: 'Juan' },
+            { nombre: 'Miguel', apellidos: 'Indurain', direccion: 'Pamplona', fnac: '10/2/1968', sexo: 'Hombre' },
+            { nombre: 'Alberto', apellidos: 'Contador', direccion: 'Madrid', fnac: '05/04/1980', sexo: 'Hombre' },
+            { nombre: 'María José', apellidos: 'Jiménez', direccion: 'Cuenca', fnac: '12/30/1981', sexo: 'Mujer' },
+        ];
+    }
 
     getNumTodos(): number {
         return this.alumnos.length;
@@ -41,6 +45,6 @@ export class AlumnoListaComponent {
     alCambiar(opcionSeleccionada: string): void {
         this.seleccion = opcionSeleccionada;
     }
-    
+
 }
 
