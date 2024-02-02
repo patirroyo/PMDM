f937a5af8661c49b5e1dec694881889cbfc9beeb
Alberto Saz <albertosaz@gmail.com>
2024-02-02 11:55:08 +0100

    con el service

--- proyectoAngular/src/app/Alumnos/alumnoLista.component.ts
+++ proyectoAngular/src/app/Alumnos/alumnoLista.component.ts
@@ -1,33 +1,40 @@
-import { Component, Inject, Input, NgModule } from '@angular/core';
+import { Component, Inject, Input, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AlumnoTituloPipe } from './alumnoTitulo.pipe';
 import { AlumnosCountComponent } from './alumnosCount.component';
 import { IAlumno, Alumno } from './alumno';
+import { AlumnosService } from './alumnosService';
 
 
 @Component({
     selector: 'lista-alumnos',
     standalone: true,
     imports: [CommonModule, RouterOutlet, AlumnoTituloPipe, AlumnosCountComponent],
+    providers: [AlumnosService],
     templateUrl: './alumnoLista.component.html',
     styleUrl: './alumno.component.css'
 })
 
 
-export class AlumnoListaComponent {
+export class AlumnoListaComponent implements OnInit{
     //recibe el valor del hijo
 
     seleccion: string = 'Todos';//Esto es para que por defecto se muestren todos los alumnos, pero se puede cambiar a Hombres o Mujeres por medio del select que envia un evento al padre(donde estamos) y se cambia el valor de la variable seleccion
     //alumnos: IAlumno[];//Esto es un array de objetos de tipo IAlumno, que es una interfaz que hemos creado en alumno.ts y tengo que importarla arriba
-    alumnos: Alumno[];
-    constructor() {
+    alumnos: Alumno[] = [];
+    /*constructor() {
         this.alumnos = [
             { nombre: 'Perico', apellidos: 'Delgado', direccion: 'Segovia', fnac: '2/28/1958', sexo: 'Hombre', nom_padre: 'Juan' },
             { nombre: 'Miguel', apellidos: 'Indurain', direccion: 'Pamplona', fnac: '10/2/1968', sexo: 'Hombre' },
             { nombre: 'Alberto', apellidos: 'Contador', direccion: 'Madrid', fnac: '05/04/1980', sexo: 'Hombre' },
             { nombre: 'María José', apellidos: 'Jiménez', direccion: 'Cuenca', fnac: '12/30/1981', sexo: 'Mujer' },
         ];
+    }*/
+    constructor(public losAlumnos: AlumnosService) {//Esto es un constructor que recibe un objeto de tipo AlumnosService, que es un servicio que hemos creado en alumnosService.ts y tengo que importar arriba y se crea por inyección de dependencias. al poner public delante de losAlumnos, se crea automáticamente el atributo DE LA CLASE y se le asigna el valor que se le pasa por parámetro
+    }
+    ngOnInit(): void {//Esto es un método que se ejecuta cuando se crea el componente sin que nadie lo llame
+        this.alumnos = this.losAlumnos.getAlumnos();
     }
 
     getNumTodos(): number {
