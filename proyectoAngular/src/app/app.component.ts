a5e6038655f6ffb3d37cbc4178eb53b0975ff678
Alberto Saz <albertosaz@gmail.com>
2024-02-08 12:18:54 +0100

    routes

--- proyectoAngular/src/app/app.component.ts
+++ proyectoAngular/src/app/app.component.ts
@@ -9,33 +9,16 @@ import { HttpClientModule } from '@angular/common/http';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, AlumnoComponent, AlumnoListaComponent, HttpClientModule],
-  template: `
-<p>{{title}}</p>
-<div>
-  <h5>Primer ejemplo</h5>
-  <h1>{{(encabezado == '')?getNombreCompleto():(5+5)}}</h1>
-  <img src='https://cordoba.salesianos.edu/wp-content/uploads/2021/05/{{imagen}}' width="100"><!--las {{}} son para que se interprete como codigo y sirve para los strings-->
-  <el-alumno></el-alumno> <!--Esto llama al componente, que tiene como atributo selector ese texto para la marca html-->
-  <button [disabled]="desactivado">Boton</button> <!--el [disabled] es para que se desactive el boton se hace con corchetes porque las {{}} son solo para strings-->
-</div>
-<br><br><h5>Segundo Ejemplo</h5>
-<div>
-  <button [class]="aplicarClases">Boton 1</button>
-  <br><br>
-  <button class="color italica negrita"[class.negrita]='false'>Boton 2</button>
-  <button class="color italica negrita"[class.negrita]='aplicarNegrita'>Boton 3</button>
-  <button class="color italica negrita"[class.negrita]='!aplicarNegrita'>Boton 4</button>
-  <br><br>
-  <button [ngClass]='addClases()'>Boton 5</button>
-</div>
-<br><br><br><h5>Tercer Ejemplo</h5>
-<div>
-  <button (click)="onClick()">Mostrar/Ocultar Bandera</button>
-  <br>
-  <img src="./assets/Flag_of_Mexico.svg" width="300" *ngIf=visible><!--el *ngIf es para que se muestre la imagen si se cumple la condicion -->
-</div>
-<br><br><br><h5>Lista Alumnos Component</h5>
-<lista-alumnos></lista-alumnos>
+  template: `              
+              <ul class="nav nav-pills ">
+                <li class="nav-item">
+                  <a class="nav-link"  href="Alumnos">Alumnos</a>
+                </li>
+                <li class="nav-item ">
+                  <a class="nav-link" href="Profesores">Profesores</a>
+                </li>
+              </ul>
+              <router-outlet></router-outlet>
 
             `,
   styleUrl: './app.component.css'
--- proyectoAngular/src/app/app.routes.ts
+++ proyectoAngular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AlumnoListaComponent } from './Alumnos/alumnoLista.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
+import { PaginaNoEncontrada } from './otros/paginaNoEncontrada';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Alumnos', component: AlumnoListaComponent },
+    { path: 'Profesores', component: ProfesoresComponent},
+    { path: '', redirectTo: '/Profesores', pathMatch: 'full'},
+    { path: '**', component: PaginaNoEncontrada},
+];
