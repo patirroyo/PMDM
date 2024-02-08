dbca57d3c0131bb36155541b004164ffcf2acf97
Alberto Saz <albertosaz@gmail.com>
2024-02-08 10:16:20 +0100

    bootsrap jquery ng create component

---
+++ proyectoAngular/src/app/otros/paginaNoEncontrada.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otros',
+  standalone: true,
+  imports: [],
+  template: '<h1>Error 404: Página no encontrada</h1>',
+  
+})
+/**
+ * Represents a page not found error.
+ */
+export class PaginaNoEncontrada {
+
+}
---
+++ proyectoAngular/src/app/profesores/profesores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profesores',
+  standalone: true,
+  imports: [],
+  templateUrl: './profesores.component.html',
+  styleUrl: './profesores.component.css'
+})
+export class ProfesoresComponent {
+
+}
