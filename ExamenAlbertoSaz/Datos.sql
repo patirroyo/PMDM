edda673979c180a3e98f96a7b46e89d0d2602d17
Alberto Saz <albertosaz@gmail.com>
2024-03-10 23:30:24 +0100

    examen entero

--- ExamenAlbertoSaz/Datos.sql
+++ ExamenAlbertoSaz/Datos.sql
@@ -19,32 +19,3 @@ INSERT Vehiculos (Matricula, Color, SerieId) VALUES ('4414 FCZ', 'Verde', 7)
 INSERT Vehiculos (Matricula, Color, SerieId) VALUES ('231-IT-2', 'Rojo', 8)
 INSERT Vehiculos (Matricula, Color, SerieId) VALUES ('1234 CBA', 'Rojo', 9)
 INSERT Vehiculos (Matricula, Color, SerieId) VALUES ('4478 FCZ', 'Blanco', 10)
-INSERT Vehiculos (Matricula, Color, SerieId) VALUES ('1232 URC', 'Gris', 1)
-
-
--- Para generar hasta 150 vehículos variados
-
--- Insertar vehículos adicionales para cada serie existente
-DECLARE @i INT = 0;
-WHILE @i < 140
-BEGIN
-    INSERT Vehiculos (Matricula, Color, SerieId)
-    VALUES 
-    (
-        -- Generar matrícula aleatoria
-        CASE 
-            WHEN @i % 3 = 0 THEN CONCAT(CHAR(65 + @i % 26), CHAR(65 + (@i + 5) % 26), '-', CONVERT(NVARCHAR, 1000 + @i % 900), '-', CHAR(65 + (@i + 10) % 26))
-            WHEN @i % 3 = 1 THEN CONCAT(CHAR(65 + (@i + 7) % 26), '-', CONVERT(NVARCHAR, 1000 + (@i + 2) % 900), '-', CHAR(65 + (@i + 15) % 26))
-            ELSE CONCAT(CHAR(65 + (@i + 5) % 26), CHAR(65 + (@i + 12) % 26), '-', CONVERT(NVARCHAR, 1000 + (@i + 3) % 900), '-', CHAR(65 + (@i + 20) % 26))
-        END,
-        -- Generar color aleatorio
-        CASE 
-            WHEN @i % 3 = 0 THEN 'Azul'
-            WHEN @i % 3 = 1 THEN 'Gris'
-            ELSE 'Negro'
-        END,
-        -- Seleccionar SerieId aleatorio
-        1 + @i % 10
-    );
-    SET @i = @i + 1;
-END;
